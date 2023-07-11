f328200f3c9c4aae329371d15c0a57ff15e2fe5e
gustavotassi <souzagustavotassi@gmail.com>
2023-07-11 00:01:41 -0300

    style: make some color corrections

--- src/components/ContactMe.tsx
+++ src/components/ContactMe.tsx
@@ -6,7 +6,7 @@ export function ContactMe() {
       href="https://cal.com/gustavo-tassi"
       target="_blank"
       rel="noreferrer"
-      className="group flex w-fit items-center justify-center gap-4 rounded-xl border-2 border-zinc-100 px-6 py-3 text-2xl"
+      className="group flex w-fit items-center justify-center gap-4 rounded-xl border-2 border-zinc-900 px-6 py-3 text-2xl dark:border-zinc-100"
     >
       Let&apos;s talk{' '}
       <ArrowRight className="text-pink-600 transition-all group-hover:translate-x-1 group-hover:text-purple-500" />
--- src/components/Social.tsx
+++ src/components/Social.tsx
@@ -10,11 +10,11 @@ export function Social() {
           href={item.link}
           target="_blank"
           rel="noreferrer"
-          className="group rounded-xl border-2 border-zinc-800 p-3 transition-all hover:border-zinc-900 dark:border-zinc-500 hover:dark:border-zinc-100"
+          className="group rounded-xl border-2 border-zinc-500 p-3 transition-all hover:border-zinc-900 dark:border-zinc-500 hover:dark:border-zinc-100"
         >
           <Image
             src={item.imageSrc}
-            className="opacity-60 transition-all group-hover:opacity-100"
+            className="opacity-60 invert-90 transition-all group-hover:opacity-100 dark:invert-0"
             alt="social media icon"
             height={32}
             width={32}
