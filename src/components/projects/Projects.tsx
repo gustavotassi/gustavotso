69662d186c37ee6869fbdaa7ea54cc73b6cf289b
gustavotassi <souzagustavotassi@gmail.com>
2023-07-15 19:54:58 -0300

    refactor: change projects section to only display a 'soon' text

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -1,19 +1,17 @@
-import Image from 'next/image'
-import excavatorImage from '@/assets/excavator.svg'
-
 export function Projects() {
   return (
     <div className="m-auto w-full max-w-7xl place-items-start px-4 pt-36 font-sans">
-      <h1 className="font-alt text-3xl font-bold leading-tight">Projects</h1>
-      <p className="text-zinc-400">What I&apos;ve done so far</p>
+      <h1 className="w-fit bg-gradient-to-r from-blue-900 to-blue-700 bg-[length:100%_10%] bg-bottom bg-no-repeat px-2 font-alt text-3xl font-bold leading-tight">
+        Projects
+      </h1>
+      <p className="pt-2 text-zinc-400">What I&apos;ve done so far</p>
 
-      <div className="flex items-center justify-center">
-        <Image
-          src={excavatorImage}
-          alt="excavator image"
-          height={512}
-          width={512}
-        />
+      <div className="flex flex-col items-center justify-center py-32">
+        <p className="text-sm font-bold uppercase tracking-widest text-zinc-700">
+          Soon <br />
+          Soon <br />
+          Soon <br />
+        </p>
       </div>
     </div>
   )
