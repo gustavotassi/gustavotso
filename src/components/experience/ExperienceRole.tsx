9c7c5ffa4a1d7014b6b4184dc8642878921b9363
gustavotassi <souzagustavotassi@gmail.com>
2023-07-15 20:18:30 -0300

    refactor: remove experience role color utils, change for clsx

--- src/components/experience/ExperienceRole.tsx
+++ src/components/experience/ExperienceRole.tsx
@@ -1,21 +1,29 @@
 'use client'
 
 import { Role } from '@/lib/enums'
-import { getExperienceRoleColor } from '@/lib/utils'
+import clsx from 'clsx'
 
 interface ExperienceRoleProps {
   role: Role
 }
 
 export function ExperienceRole({ role }: ExperienceRoleProps) {
-  const experienceRoleColor = getExperienceRoleColor(role)
-
   return (
     <div
-      className={`flex w-fit items-center justify-center gap-3 rounded-full bg-${experienceRoleColor}-900/40 px-4 py-1`}
+      className={clsx(`flex w-fit items-center justify-center gap-3 rounded-full px-4 py-1`, {
+        'bg-red-900/40': role === Role.BACK,
+        'bg-blue-900/40': role === Role.FRONT,
+        'bg-purple-900/40': role === Role.FULL,
+        'bg-green-900/40': role === Role.MOBILE
+      })}
     >
       <div
-        className={`m-auto h-2 w-2 rounded-full bg-${experienceRoleColor}-500`}
+        className={clsx(`m-auto h-2 w-2 rounded-full`, {
+          'bg-red-500': role === Role.BACK,
+          'bg-blue-500': role === Role.FRONT,
+          'bg-purple-500': role === Role.FULL,
+          'bg-green-500': role === Role.MOBILE
+        })}
       />
       <p>{role}</p>
     </div>
